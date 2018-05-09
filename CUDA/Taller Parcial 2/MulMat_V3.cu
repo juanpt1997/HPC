03b1e72cf73b1b03c9aa0540c2d1c6924598a944
juanpt1997 <tabaresricojuanpablo@utp.edu.co>
2018-05-09 16:18:01 -0500

    version grande

--- CUDA/Taller Parcial 2/MulMat_V3.cu
+++ CUDA/Taller Parcial 2/MulMat_V3.cu
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <cuda.h>
-#define TILE_DIM 85
+#define TILE_DIM 84
 
 __global__ void MulMatriz(float *m1, float *m2, float *mr, int fil1, int col1,int fil2, int col2) {
 	
